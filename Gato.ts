1d9353c960e8d689565631cb418a721b5fb086a1
jazmin-avila <jazmin.avila@axity.com>
2025-02-10 10:36:38 -0600

    Primer cambio

--- Gato.ts
+++ Gato.ts
@@ -1,12 +1,16 @@
 class Gato {
     nombre: string;
     sexo: string;
+    age:number;
+    weight:number;
     color: string;
     textura: string;
 
-    constructor(nombre: string, sexo: string, color: string, textura: string) {
+    constructor(nombre: string, sexo: string, age:number, weight:number, color: string, textura: string) {
         this.nombre = nombre;
         this.sexo = sexo;
+        this.age = age;
+        this.weight = weight;
         this.color = color;
         this.textura = textura;
     }
@@ -14,11 +18,14 @@ class Gato {
     mostrarInformacion(): void {
         console.log(`Nombre: ${this.nombre}`);
         console.log(`Sexo: ${this.sexo}`);
+        console.log(`age: ${this.sexo}`);
+        console.log(`weight: ${this.sexo}`);
         console.log(`Color: ${this.color}`);
         console.log(`Textura: ${this.textura}`);
     }
 }
 
-const gato1 = new Gato("Felix", "Macho", "Naranja", "Suave");
+const gato1 = new Gato("Oscar", "Macho", 3, 7, "Marron", "Rayada");
+const gato2 = new Gato("Luna", "Hembra", 2, 5, "Gris", "Lisa");
 
 gato1.mostrarInformacion();
